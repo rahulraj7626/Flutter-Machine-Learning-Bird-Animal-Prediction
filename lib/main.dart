9f2a46937013e557f7bf9b998589d068fbb60e2a
rahulraj7626 <rahulraj7626@gmail.com>
2021-12-14 17:03:35 +0530

    final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fluttermlprediction/tensorflowfuntions.dart';
 import 'package:fluttermlprediction/ui.dart';
 
 void main() {
--- lib/ui.dart
+++ lib/ui.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
 
 class UI extends StatefulWidget {
   const UI({Key? key}) : super(key: key);
@@ -9,75 +13,215 @@ class UI extends StatefulWidget {
 }
 
 class _UIState extends State<UI> {
-  var image1 =
-      'https://i.picsum.photos/id/1005/5760/3840.jpg?hmac=2acSJCOwz9q_dKtDZdSB-OIK1HUcwBeXco_RMMTUgfY';
-  var image2 =
-      'https://i.picsum.photos/id/1003/1181/1772.jpg?hmac=oN9fHMXiqe9Zq2RM6XT-RVZkojgPnECWwyEF1RvvTZk';
+  List? _outputs;
+  XFile? _image;
+  bool _loading = false;
+
+  final ImagePicker _picker = ImagePicker();
+  @override
+  void initState() {
+    super.initState();
+    _loading = true;
+
+    loadModel().then((value) {
+      setState(() {
+        _loading = false;
+      });
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+      model: "assets/model_unquant.tflite",
+      labels: "assets/labels.txt",
+      numThreads: 1,
+    );
+  }
+
+  classifyImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+        path: image.path,
+        imageMean: 0.0,
+        imageStd: 255.0,
+        numResults: 2,
+        threshold: 0.2,
+        asynch: true);
+    setState(() {
+      _loading = false;
+      _outputs = output;
+    });
+  }
+
+  @override
+  void dispose() {
+    Tflite.close();
+    super.dispose();
+  }
+
+  Future getImageCamera() async {
+    var image =
+        await _picker.pickImage(source: ImageSource.camera, imageQuality: 50);
+    if (image == null) return null;
+    setState(() {
+      _loading = true;
+      _image = image;
+    });
+    classifyImage(File(_image!.path));
+  }
+
+  Future getImageGallery() async {
+    var image =
+        await _picker.pickImage(source: ImageSource.gallery, imageQuality: 50);
+    if (image == null) return null;
+    setState(() {
+      _loading = true;
+      _image = image;
+    });
+    classifyImage(File(_image!.path));
+  }
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIOverlays([]);
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            children: [
-              Expanded(
-                child: Column(
-                  children: const [
-                    Text(
-                      "Animal Bird Ditection",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Example of Machine Learning Model integrated with Flutter',
-                      textAlign: TextAlign.center,
-                    )
-                  ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: const [
+                      Text(
+                        "Animal Bird Detection",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Example of Machine Learning Model integrated with Flutter',
+                        textAlign: TextAlign.center,
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              Expanded(
-                flex: 9,
-                child: Container(
-                  margin: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: NetworkImage(
-                          image1,
+                Expanded(
+                  flex: 9,
+                  child: _image == null
+                      ? Container(
+                          margin: const EdgeInsets.all(10),
+                          decoration: const BoxDecoration(
+                            color: Color(0xFFD4DCFF),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(25.0)),
+                          ),
+                          child: Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Container(
+                              margin: const EdgeInsets.all(20),
+                              decoration: BoxDecoration(
+                                  color: Colors.black26,
+                                  borderRadius: BorderRadius.circular(15)),
+                              padding: const EdgeInsets.all(20),
+                              child: const Text(
+                                "Upload an image",
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        )
+                      : Container(
+                          margin: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: FileImage(File(_image!.path)),
+                                fit: BoxFit.cover),
+                            color: Colors.transparent,
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(25.0)),
+                          ),
+                          child: Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Container(
+                              margin: const EdgeInsets.all(20),
+                              decoration: BoxDecoration(
+                                  color: Colors.black26,
+                                  borderRadius: BorderRadius.circular(15)),
+                              padding: const EdgeInsets.all(20),
+                              child: Text(
+                                _outputs?[0]["label"] ?? "",
+                                style: const TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.white),
+                              ),
+                            ),
+                          ),
                         ),
-                        fit: BoxFit.cover),
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.all(Radius.circular(25.0)),
-                  ),
                 ),
-              ),
-              Expanded(
-                flex: 2,
-                child: Card(
-                  margin: EdgeInsets.all(0),
-                  color: Colors.grey.shade500,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(15.0),
-                      topLeft: Radius.circular(15.0),
-                    ),
-                  ),
-                  child: Container(
-                    width: double.infinity,
-                    decoration: const BoxDecoration(
+                Expanded(
+                  flex: 2,
+                  child: Card(
+                    margin: const EdgeInsets.all(0),
+                    color: const Color(0xFFD4DCFF),
+                    shape: const RoundedRectangleBorder(
                       borderRadius: BorderRadius.only(
                         topRight: Radius.circular(15.0),
                         topLeft: Radius.circular(15.0),
                       ),
                     ),
-                    child: Text("Hiiiii"),
+                    child: Container(
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(15.0),
+                            topLeft: Radius.circular(15.0),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.all(10),
+                              decoration: const BoxDecoration(
+                                color: Color(0xFF65708F),
+                                shape: BoxShape.circle,
+                              ),
+                              child: IconButton(
+                                  onPressed: () {
+                                    getImageCamera();
+                                  },
+                                  icon: const Icon(
+                                    Icons.camera_alt,
+                                    color: Colors.white,
+                                  )),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.all(10),
+                              decoration: const BoxDecoration(
+                                color: Color(0xFF65708F),
+                                shape: BoxShape.circle,
+                              ),
+                              child: IconButton(
+                                  onPressed: () {
+                                    getImageGallery();
+                                  },
+                                  icon: const Icon(
+                                    Icons.image,
+                                    color: Colors.white,
+                                  )),
+                            )
+                          ],
+                        )),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
